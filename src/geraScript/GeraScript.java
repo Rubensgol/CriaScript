e229277753609cb1a2bf2d4dee394e3ecdd1b82f
rubensgol <aralis.silva@hotmail.com>
2021-07-11 22:57:33 -0300

    mudança na classe criando apenas um metodo estatico e criando a classe de teste com 5 testes

--- src/geraScript/GeraScript.java
+++ src/geraScript/GeraScript.java
@@ -1,46 +1,16 @@
 package geraScript;
 
 public class GeraScript {
-	private String nomeCampo;
-	private Tipos tipo;
-	
-	private boolean nulo;
-	private boolean auto_increment;
-	public GeraScript(String nomeCampo, Tipos tipo) {
-		this.nomeCampo=nomeCampo;
-		this.tipo=tipo;
-	}
-	public GeraScript autoIncrementa(boolean autoIncrement) {
-		this.auto_increment=autoIncrement;
-		return this;
-	}
-	public GeraScript eNulo(boolean isnulo) {
-		this.nulo=isnulo;
-		return this;
-	}
-	public String getNomeCapo() {
-		return nomeCampo;
-	}
-	public void setNomeCapo(String nomeCapo) {
-		this.nomeCampo = nomeCapo;
-	}
-	public boolean isNulo() {
-		return nulo;
-	}
-	public void setNulo(boolean nulo) {
-		this.nulo = nulo;
-	}
-	public boolean isAuto_increment() {
-		return auto_increment;
-	}
-	public void setAuto_increment(boolean auto_increment) {
-		this.auto_increment = auto_increment;
-	}
-	public Tipos getTipo() {
-		return tipo;
-	}
-	public void setTipo(Tipos tipo) {
-		this.tipo = tipo;
+	public static String geraOScript(String nomeCampo, Tipos tipo, boolean auto_increment, boolean notNulo) {
+		String script="";
+		script+="'"+nomeCampo+"'";
+		script+=" "+tipo;
+		if(notNulo)
+			script+=" NOT NULL";
+		if(auto_increment)
+			script+=" AUTO_INCREMENT";
+		script+=",";
+		return script;
 	}
 
 }
